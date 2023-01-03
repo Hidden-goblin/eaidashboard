2aa53aab2262958ea45a784797e63934407afc71
Eric Aï <hiddengob@free.fr>
2023-01-03 22:38:28 +0100

    Fix postgresql connection link (#4)

--- app/api.py
+++ app/api.py
@@ -22,7 +22,7 @@ Eaidashboard is a simple api and front to monitor test activities.
 """
 app = FastAPI(title="Eaidashboard",
               description=description,
-              version="1.5",
+              version="1.6",
               license_info={
                   "name": "GNU GPL v3",
                   "url": "https://www.gnu.org/licenses/gpl-3.0.en.html"
--- dockerfile
+++ dockerfile
@@ -1,13 +1,13 @@
 FROM python:3.11-alpine
 
-
 RUN apk update &&  pip install pipenv
 
 WORKDIR /usr/src/dashboard
 
 COPY Pipfile main.py ./
-COPY app app/
 
 RUN pipenv install
+
+COPY app app/
 
 CMD pipenv run python main.py
